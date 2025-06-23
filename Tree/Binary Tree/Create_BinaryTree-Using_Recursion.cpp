bea609041a7f21ee5c5995c52c537b00cc462187
Pradip-Garai <pradipgarai173@gmail.com>
2025-06-23 16:40:29 +0530

    Create Binary Tree Using Recursion

---
+++ Tree/Binary Tree/Create_BinaryTree-Using_Recursion.cpp
@@ -0,0 +1,39 @@
+// Create a Binary Tree Using Recursion
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+      int data;
+      Node *left,*right;
+    public:
+      Node(int data){
+        this->data=data;
+        left=right=NULL;
+      }
+};
+
+Node* BinaryTree(){
+    int x;
+    cin>>x;
+    if(x==-1)
+      return NULL;
+
+    Node *temp = new Node(x);
+
+    cout<<"Enter Left Child of "<<x<<" : ";
+    temp->left = BinaryTree();
+
+    cout<<"Enter Right Child of "<<x<<" : ";
+    temp->right = BinaryTree();
+
+    return temp;
+}
+
+int main(){
+
+    Node *root;
+    cout<<"Enter Root Node: ";
+    root = BinaryTree();
+    return 0;
+}
